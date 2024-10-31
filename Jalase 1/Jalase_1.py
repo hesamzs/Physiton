f753fe3fe164bd9799725e660347d6ed16cfa195
HesamZs <javad.zanooshe@gmail.com>
2024-10-31 17:45:55 +0330

    Jalase 1 | 2024-10-30

---
+++ Jalase 1/Jalase_1.py
@@ -0,0 +1,31 @@
+# # print("Hello World")
+# # 
+
+# x = 1 # Integer int
+# print(x)
+x = 5
+# print(x)
+
+# x = "sd" # String str
+# print("x")
+
+# #\
+
+x = x + 1 # 6
+print(x)
+
+y = x * 5
+z = 15 / 3
+p = 2 ** 2
+
+print(y,z,p)
+print("This is Y : ",y)
+
+x = [12,50,17,119]
+y = [20]
+print(x+y)
+
+x.append(90)
+# x.insert(*,*)
+print(x)
+print(x[4])
